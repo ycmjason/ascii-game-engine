492c7a846474b36480fafdd74d4cd35b5558f244
Jason Yu <ycm.jason@gmail.com>
2016-05-21 12:56:09 +0100

    adding key events, and fixed negative mod issues

--- test-server/js/block-component.js
+++ test-server/js/block-component.js
@@ -1,7 +1,8 @@
-function Block(){
+function Block(startingPosition){
   this.representations = [' o\nooo', 'o\noo\no', 'ooo\n o', ' o\noo\n o'];
   this.state = 0;
-  this.position = [0, 0];
+  this.position = startingPosition || [0, 0];
+  this.z_index = 0;
 }
 Block.prototype.tick = function(currentTick){
   if(currentTick % 60 == 0) {
@@ -10,4 +11,23 @@ Block.prototype.tick = function(currentTick){
 }
 Block.prototype.toString = function(){
   return this.representations[this.state];
+}
+Block.prototype.keydown = function(key){
+  switch(key.name){
+    case 'up':
+      this.state = (this.state + 1) % 4
+      break;
+    case 'down':
+      this.position[1] += 1;
+      break;
+    case 'space':
+      this.position[1] = 48;
+      break;
+    case 'left':
+      this.position[0] -= 1;
+      break;
+    case 'right':
+      this.position[0] += 1;
+      break;
+  }
 }
